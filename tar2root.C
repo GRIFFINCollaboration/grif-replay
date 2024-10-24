2beefa3d947cda8ded81c86de0de85aa2693cbe0
Adam Garnsworthy <garnznation@gmail.com>
2024-10-24 08:02:21 -0700

    Fixed tar2root script to handle compressed format.

--- tar2root.C
+++ tar2root.C
@@ -1,3 +1,4 @@
+// gSystem->Load("/usr/lib64/libz.so")
 // .x tar2root.C+("test.tar","test.root")
 //
 // radware matrices were always 4k and 2bytes[.mat]  [later 4 bytes for .m4b]
@@ -5,6 +6,7 @@
 #include "TFile.h"
 #include "TH1F.h"
 #include "TH2F.h"
+#include <zlib.h>
 
 #define FILE_BUFSIZ    1048576 // 64k * 4 * 4 [1M]
 #define MAX_SPECTRA      65535
@@ -21,14 +23,87 @@ typedef struct tar_file_header_struct { // should be 512 bytes
 
 Tar_file_head file_head;
 int           file_body[128*FILE_BUFSIZ];
+int           decomp_buf[128*FILE_BUFSIZ];
+
+unsigned char *file_ptr   = (unsigned char *)file_body;
+unsigned char *decomp_ptr = (unsigned char *)decomp_buf;
 
 TH1F *spectrum[MAX_SPECTRA];
 TH2F   *spec2d[MAX_2D];
 
+//extern int R__Inflate(unsigned char *ibuf, long *ibufcnt, unsigned char *obuf, long *obufcnt);
+
+//extern void R__unzipZLIB(int *ibufcnt, unsigned char *ibuf, int *obufcnt, unsigned char *obuf);
+
+#define COMPRESS_BUFSIZ 270000000
+// allow for 8k^2 = 65M-chan * 4bytes = 256Mbytes [~270000000]
+static char compress_buf[COMPRESS_BUFSIZ];
+static int *comp_ptr = (int *)compress_buf;
+int compress_buffer(char *input, int size)
+{
+   z_stream strm;
+   int status;
+
+   strm.zalloc = Z_NULL;  strm.zfree = Z_NULL;  strm.opaque = Z_NULL;
+   if( (status = deflateInit(&strm, Z_DEFAULT_COMPRESSION)) != Z_OK ){
+      fprintf(stderr,"zlib compression error\n"); return(-1);
+   }
+   strm.avail_in = size;
+   strm.next_in = (Bytef *)input;
+   
+   strm.avail_out = COMPRESS_BUFSIZ; // should never be filled
+   strm.next_out = (Bytef *)compress_buf;
+   deflate(&strm, Z_FINISH);
+   status = COMPRESS_BUFSIZ - strm.avail_out;
+   deflateEnd(&strm);
+   return(status);
+}
+int decompress_buffer(char *input, int size)
+{
+   z_stream strm;
+   int status;
+
+   strm.zalloc = Z_NULL;  strm.zfree = Z_NULL;  strm.opaque = Z_NULL;
+   strm.avail_in = 0;   strm.next_in = Z_NULL; 
+   if( (status = inflateInit(&strm)) != Z_OK ){
+      fprintf(stderr,"zlib decompression error\n"); return(-1);
+   }
+   strm.avail_in = size;
+   strm.next_in = (Bytef *)input;
+   
+   strm.avail_out = COMPRESS_BUFSIZ; // should never be filled
+   strm.next_out = (Bytef *)compress_buf;
+   inflate(&strm, Z_NO_FLUSH);
+   status = COMPRESS_BUFSIZ - strm.avail_out;
+   inflateEnd(&strm);
+   return(status);
+}
+
+int read_entry(FILE *fp, int *pad)
+{
+   char tmp[256];
+   int size;
+   
+   if( fread( &file_head, 512, 1, fp) < 1 ){ return(-1); }
+   memcpy(tmp, file_head.size, 12); tmp[12]=0;
+   printf("size:%s\n", tmp);
+   if( sscanf(tmp, "%o", &size) < 1 ){
+      fprintf(stderr,"can't read histo size from:%s\n", file_head.size);
+   }
+   *pad = (512 - (size % 512)) % 512; // pad to multiple of 512 bytes
+   printf("size+pad:%d\n", size+ *pad);
+   if( fread( &file_body, sizeof(char), size+ *pad, fp) <  size+ *pad ){
+      fprintf(stderr,"short read on histo:%s[%d]\n", file_head.name, size);
+      return(-1);
+   }
+   return(size);
+}
+
 int tar2root(char *tarfile, char *rootfile)
 {
-   int i, j, pad, count1, count2, size, xbins, ybins, len;
+   int i, j, pad, count1, count2, size, xbins, ybins, len, status;
    char tmp[256], handle[128], title[128], fname[64];
+   long isize, osize;
    FILE *fp, *tmp_fp;
 
    TFile *outf = new TFile(rootfile, "RECREATE","Root Histograms");
@@ -46,18 +121,19 @@ int tar2root(char *tarfile, char *rootfile)
    }   
    count1 = count2 = 0;
    while( 1 ){
-      if( fread( &file_head, 512, 1, fp) < 1 ){ break; }
-      memcpy(tmp, file_head.size, 12); tmp[12]=0;
-      printf("size:%s\n", tmp);
-      if( sscanf(tmp, "%o", &size) < 1 ){
-         fprintf(stderr,"can't read histo size from:%s\n", file_head.size);
-      }
-      pad = (512 - (size % 512)) % 512; // pad to multiple of 512 bytes
-      printf("size+pad:%d\n", size+pad);
-      if( fread( &file_body, sizeof(char), size+pad, fp) <  size+pad ){
-         fprintf(stderr,"short read on histo:%s[%d]\n", file_head.name, size);
-         break;
-      }
+      //if( fread( &file_head, 512, 1, fp) < 1 ){ break; }
+      //memcpy(tmp, file_head.size, 12); tmp[12]=0;
+      //printf("size:%s\n", tmp);
+      //if( sscanf(tmp, "%o", &size) < 1 ){
+      //   fprintf(stderr,"can't read histo size from:%s\n", file_head.size);
+      //}
+      //pad = (512 - (size % 512)) % 512; // pad to multiple of 512 bytes
+      //printf("size+pad:%d\n", size+pad);
+      //if( fread( &file_body, sizeof(char), size+pad, fp) <  size+pad ){
+      //   fprintf(stderr,"short read on histo:%s[%d]\n", file_head.name, size);
+      //   break;
+      //}
+      if( (size = read_entry(fp, &pad)) == -1 ){ break; }
       if( strcmp(file_head.name, "config_file") == 0 ){ continue; }
       memcpy(tmp, file_head.uid, 8); tmp[8]=0;
       if( sscanf(tmp, "%o", &xbins) < 1 ){
@@ -79,11 +155,24 @@ int tar2root(char *tarfile, char *rootfile)
       } else {
          spec2d[count2]= new TH2F(file_head.name,file_head.link,xbins,0,xbins,ybins,0,ybins);
          if( size+pad > 0 ){
-            for(j=0; j<ybins; j++){ for(i=0; i<xbins; i++){
-                  spec2d[count2]->SetBinContent(i,j,file_body[i+j*xbins]);
-            }}
+            if( xbins*ybins > 65536 ){
+               //isize = size; osize = 128*FILE_BUFSIZ; pad = osize;
+               //R__Inflate(file_ptr, &isize, decomp_ptr, &osize );
+               //R__unzip(&size, file_ptr, &pad, decomp_ptr, &status );
+               //size = size+9; pad=128*FILE_BUFSIZ;
+               //R__unzipZLIB(&size, file_ptr-9, &pad, decomp_ptr );
+               size = decompress_buffer((char *)file_body, size);
+               printf("DECOMP:Size=%d\n", size);
+               for(j=0; j<ybins; j++){ for(i=0; i<xbins; i++){
+                     spec2d[count2]->SetBinContent(i,j,comp_ptr[i+j*xbins]);
+               }}
+            } else {
+               for(j=0; j<ybins; j++){ for(i=0; i<xbins; i++){
+                     spec2d[count2]->SetBinContent(i,j,file_body[i+j*xbins]);
+               }}
+            }
+            spec2d[count2++]->Write();
          }
-         spec2d[count2++]->Write();
       }
    }
    fclose(fp);
